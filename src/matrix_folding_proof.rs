185c8137a8cfa223eed36d9e00f443fbb6bdeb6a
Aram Lindroth <apad201@gmail.com>
2022-07-22 13:46:01 -0400

    finished proof generation method

--- src/matrix_folding_proof.rs
+++ src/matrix_folding_proof.rs
@@ -20,16 +20,16 @@ pub struct ZKMatrixFoldingProof {
     pub(crate) R_vec1: Vec<CompressedRistretto>,
     pub(crate) L_vec2: Vec<CompressedRistretto>,
     pub(crate) R_vec2: Vec<CompressedRistretto>,
-    pub(crate) L_vec3: Vec<CompressedRistretto>,
-    pub(crate) R_vec3: Vec<CompressedRistretto>,
+    pub(crate) L_vec3_beta: Vec<CompressedRistretto>,
+    pub(crate) R_vec3_beta: Vec<CompressedRistretto>,
+    pub(crate) L_vec3_tau: Vec<CompressedRistretto>,
+    pub(crate) R_vec3_tau: Vec<CompressedRistretto>,
     pub(crate) alpha: CompressedRistretto,
     pub(crate) beta: CompressedRistretto,
     pub(crate) sigma: CompressedRistretto,
     pub(crate) tau: CompressedRistretto,
     pub(crate) rho_vec: Vec<CompressedRistretto>,
-    pub(crate) z_vec: Vec<Scalar>,
-    pub(crate) a: Scalar,
-    pub(crate) b: Scalar,
+    pub(crate) z_vec: Vec<Scalar>
 
     // These are for debugging purposes only, will be removed lated
     /* pub(crate) TESTx: Vec<Scalar>,
@@ -51,7 +51,7 @@ impl ZKMatrixFoldingProof {
         mut G_vec: Vec<RistrettoPoint>, // say these are stored in row-major
         mut H_vec: Vec<RistrettoPoint>,
         mut U_vec: Vec<RistrettoPoint>, // this used to be Q and was not a vector nor mutable
-        g_0: RistrettoPoint, // blinding group element
+        g_0: RistrettoPoint, // blinding group elements
         mut a_vec: Vec<Scalar>, // A contains the TRANSPOSE of the matrix that is being used!!
         mut b_vec: Vec<Scalar>,
         mut c_vec: Vec<Scalar>,
@@ -102,12 +102,10 @@ impl ZKMatrixFoldingProof {
         let mut R_vec1 = Vec::with_capacity(lg_m);
         let mut L_vec2 = Vec::with_capacity(lg_n);
         let mut R_vec2 = Vec::with_capacity(lg_n);
-        let mut L_vec3 = Vec::with_capacity(lg_k);
-        let mut R_vec3 = Vec::with_capacity(lg_k);
-
-        // create vectors for final ZK reduction
-        let mut rho_vec = Vec::with_capacity(4);
-        let mut z_vec = Vec::with_capacity(5);
+        let mut L_vec3_beta = Vec::with_capacity(lg_k);
+        let mut R_vec3_beta = Vec::with_capacity(lg_k);
+        let mut L_vec3_tau = Vec::with_capacity(lg_k);
+        let mut R_vec3_tau = Vec::with_capacity(lg_k);
 
         // Debugging: store challenges explicitly so we can check to be sure challenges
         // are recovered properly in verification.
@@ -196,6 +194,7 @@ impl ZKMatrixFoldingProof {
         while n != 1 {
             n = n/2;
             let (a_t, a_b) = a.split_at_mut(n);
+            let (c_t, c_b) = c.split_at_mut(n*k);
             let (G_t, G_b) = G.split_at_mut(n);
             let (U_t, U_b) = U.split_at_mut(n*k);
 
@@ -228,7 +227,7 @@ impl ZKMatrixFoldingProof {
 
              // get challenge and its inverse
             let y = transcript.challenge_scalar(b"y");
-            let y_inv = x.invert();
+            let y_inv = y.invert();
             
             // compute new a, G
             for i in 0..n {
@@ -271,19 +270,136 @@ impl ZKMatrixFoldingProof {
         transcript.append_point(b"s", &sigma.clone().compress());
         transcript.append_point(b"t", &tau.clone().compress());
 
+        
+        // now fold k/B
+        while k != 1 {
+            k = k/2;
+            let (b_l, b_r) = b.split_at_mut(k);
+            let (c_l, c_r) = c.split_at_mut(k); // n == 1 by the time we get here
+            let (H_l, H_r) = H.split_at_mut(k);
+            let (U_l, U_r) = U.split_at_mut(k);
 
-        // return value
-        // note proof keeps its own record of L and R terms from each iteration, separate from transcript
+            // no need to compute cross-term exponents: AB_l is just C_l
+
+            // blinding
+            let r_l = Scalar::random(&mut rng);
+            let r_r = Scalar::random(&mut rng);
+            let s_l = Scalar::random(&mut rng);
+            let s_r = Scalar::random(&mut rng);
+
+            let L_beta = RistrettoPoint::vartime_multiscalar_mul(
+                b_l.iter().chain(iter::once(&r_l)), 
+                H_r.iter().chain(iter::once(&g_0))
+            ).compress(); 
+
+            let R_beta = RistrettoPoint::vartime_multiscalar_mul(
+                b_r.iter().chain(iter::once(&r_r)), 
+                H_l.iter().chain(iter::once(&g_0))
+            ).compress();
+
+            let L_tau = RistrettoPoint::vartime_multiscalar_mul(
+                c_l.iter().chain(iter::once(&s_l)), 
+                U_r.iter().chain(iter::once(&g_0))
+            ).compress(); 
+
+            let R_tau = RistrettoPoint::vartime_multiscalar_mul(
+                c_r.iter().chain(iter::once(&s_r)), 
+                U_l.iter().chain(iter::once(&g_0))
+            ).compress();
+            
+            L_vec3_beta.push(L_beta);
+            R_vec3_beta.push(R_beta);
+            L_vec3_tau.push(L_tau);
+            R_vec3_tau.push(R_tau);
+
+            // add Ls and Rs to transcript
+            transcript.append_point(b"bL", &L_beta);
+            transcript.append_point(b"bR", &R_beta);  
+            transcript.append_point(b"tL", &L_tau);
+            transcript.append_point(b"tR", &R_tau);  
+
+            // get challenge and its inverse
+            let z = transcript.challenge_scalar(b"z");
+            let z_inv = z.invert();
+
+            // compute new b, H
+            for i in 0..k {
+                b_l[i] = z * b_l[i] + b_r[i];
+                H_l[i] = RistrettoPoint::vartime_multiscalar_mul(&[z_inv, one], &[H_l[i], H_r[i]]);
+            }
+
+            // compute new c, U
+            for i in 0..k { // n == 1
+                c_l[i] = z * c_l[i] + c_r[i];
+                U_l[i] = RistrettoPoint::vartime_multiscalar_mul(&[z_inv, one], &[U_l[i], U_r[i]]);
+            }
+            // compute + update blindings
+            r = (z * r_l) + r + (z_inv * r_r);
+            s = (z * s_l) + s + (z_inv * s_r);
+
+            // update values
+            b = b_l;
+            c = c_l;
+            H = H_l;
+            U = U_l;
+        }
+
+        // final ZK scalar mult step
+
+        // compute blinding factors and commitments
+        let t: Vec<_> = (0..5).map(|_| Scalar::random(&mut rng)).collect();
+        let rho_0 = RistrettoPoint::vartime_multiscalar_mul(
+            iter::once(&t[0]).chain(iter::once(&t[1])), 
+            G.iter().chain(iter::once(&g_0))
+        ).compress();
+        let rho_1 = RistrettoPoint::vartime_multiscalar_mul(
+            iter::once(&t[2]).chain(iter::once(&t[3])), 
+            H.iter().chain(iter::once(&g_0))
+        ).compress();
+        let rho_2 = RistrettoPoint::vartime_multiscalar_mul(
+            iter::once(&(t[0] * t[2])).chain(iter::once(&t[4])), 
+            U.iter().chain(iter::once(&g_0))
+        ).compress();
+        let rho_3 = RistrettoPoint::vartime_multiscalar_mul(
+            iter::once(&(t[0] * b[0] + t[2] * a[0])), 
+            U.iter()
+        ).compress();
+
+        let rho_vec = vec![rho_0, rho_1, rho_2, rho_3];
+
+        // add commitments to transcript
+        for comm in rho_vec.iter() {
+            transcript.append_point(b"rh", comm);
+        }
+
+        // get challenge 
+        let x = transcript.challenge_scalar(b"x");
+
+        // compute responses
+        let z_0 = t[0] + (a[0] * x);
+        let z_1 = t[1] + (q * x);
+        let z_2 = t[2] + (b[0] * x);
+        let z_3 = t[3] + (r * x);
+        let z_4 = t[4] + (s * x * x);
+
+        let z_vec = vec![z_0, z_1, z_2, z_3, z_4];
+
+        // package everything up into the struct to return, compressing as needed
         ZKMatrixFoldingProof {
-            L_vec1: L_vec1,
-            R_vec1: R_vec1,
-            L_vec3: L_vec3,
-            R_vec3: R_vec3,
-            L_vec2: L_vec2,
-            R_vec2: R_vec2,
-            a: a[0],
-            b: b[0],
-
+            L_vec1,
+            R_vec1,
+            L_vec2,
+            R_vec2,
+            L_vec3_beta,
+            R_vec3_beta,
+            L_vec3_tau,
+            R_vec3_tau,
+            alpha: alpha.compress(),
+            beta: beta.compress(),
+            sigma: sigma.compress(),
+            tau: tau.compress(),
+            rho_vec,
+            z_vec
             // fields below will be removed, for debugging only
             /* TESTx: TESTchall,
             TESTGf: G[0],
